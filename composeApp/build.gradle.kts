7e7412ed54382a1d29fd6d51dde72ac0d01c26b5
Yojit Ghadi <yojitghadi@outlook.com>
2025-12-23 23:32:50 +0530

    Update build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -23,6 +23,7 @@ kotlin {
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
+            implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
