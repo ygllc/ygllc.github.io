df1a6048c95578e1d8b0416342fd32bdeb7e887b
Yojit Ghadi <yojitghadi@outlook.com>
2025-12-23 23:18:26 +0530

    Update build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -29,6 +29,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.material3)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
